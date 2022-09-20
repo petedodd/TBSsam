4e9620dfc45fb50bc0798c23b856b4497f6f97bb
Pete Dodd <p.j.dodd@sheffield.ac.uk>
2022-09-20 16:12:56 +0100

    TBSpeed scores coded

--- R/2modeloutcomes.R
+++ R/2modeloutcomes.R
@@ -17,9 +17,6 @@ load(file=gh('data/POPS0.Rdata'))
 ## --- WHO
 pop <- POPS[["WHO_notTB"]]
 pop <- appendWHOscores(pop)
-names(pop)
-## TODO need contact here too
-
 pop0 <- POPS0[["WHO_notTB"]]
 pop0 <- appendWHOscores(pop0)
 popt <- POPS[["WHO_TB"]]
@@ -27,37 +24,55 @@ popt <- appendWHOscores(popt)
 popt0 <- POPS0[["WHO_TB"]]
 popt0 <- appendWHOscores(popt0)
 
+
 ## --- TB Speed
-pop <- POPS[["TBS_notTB"]]
-pop <- appendTBSscores(pop)
-names(pop)
-## BUG   object 'Contact_with_adult_TB_case' not found
+spop <- POPS[["TBS_notTB"]]
+spop <- appendTBSscores(spop)
+spop0 <- POPS0[["TBS_notTB"]]
+spop0 <- appendTBSscores(spop0)
+spopt <- POPS[["TBS_TB"]]
+spopt <- appendTBSscores(spopt)
+spopt0 <- POPS0[["TBS_TB"]]
+spopt0 <- appendTBSscores(spopt0)
 
 
 ## compare
-pop0[,method:='no correlation']
-pop[,method:='copulas']
-popt0[,method:='no correlation']
-popt[,method:='copulas']
-pop0[,TB:='not TB']
-pop[,TB:='not TB']
-popt0[,TB:='TB']
-popt[,TB:='TB']
-CF <- rbindlist(list(pop,popt,pop0,popt0))
+pop0[,method:='no correlation']; spop0[,method:='no correlation']
+pop[,method:='copulas']; spop[,method:='copulas']
+popt0[,method:='no correlation']; spopt0[,method:='no correlation']
+popt[,method:='copulas']; spopt[,method:='copulas']
+pop0[,TB:='not TB']; spop0[,TB:='not TB']
+pop[,TB:='not TB']; spop[,TB:='not TB']
+popt0[,TB:='TB']; spopt0[,TB:='TB']
+popt[,TB:='TB']; spopt[,TB:='TB']
+CF <- rbindlist(list(pop,popt,pop0,popt0)) #WHO version
+sCF <- rbindlist(list(spop,spopt,spop0,spopt0)) #TBS version
 
-## ggplot(CF,aes(score_noX,y=..density..,col=method))+
-##   geom_density()+
-##   facet_wrap(~TB)
+## summary WHO
+CFS <- CF[,.(CXR=mean(score_X),noCXR=mean(score_noX),
+             CXR.sd=sd(score_X),noCXR.sd=sd(score_noX)),
+          by=.(TB,method)][order(TB)] #very similar
+CFS
+fwrite(CFS,file=here('data/compare.summary.WHO.csv'))
 
-CF[,mean(score_X),by=.(method,TB)] #very similar
-CF[,sd(score_X),by=.(method,TB)] #very similar
 
+## summary TBS
+sCFS <- sCF[,.(TBS1S=mean(TBS1S),TBS2Sa=mean(TBS2Sa),TBS2Sb=mean(TBS2Sb),
+               TBS1S.sd=sd(TBS1S),TBS2Sa.sd=sd(TBS2Sa),TBS2Sb.sd=sd(TBS2Sb)),
+          by=.(TB,method)][order(TB)] #very similar
+sCFS
+fwrite(sCFS,file=here('data/compare.summary.TBS.csv'))
 
+## https://tbksp.org/en/node/2032
+## https://docs.google.com/presentation/d/1dTrmzyfHa0KAja2ODXiQvRnpTYj1xmD2/edit#slide=id.p8
+## 
 ## TODO
-## // code TBS scores
-## // comparison of score histograms w/ & w/o correlations accounted for
 ## cost data template
 ## cost & treatment outcomes
 ## CFRs & DALYs
 ## mixture of TB vs not TB
 ## HE outcomes
+
+
+
+
--- R/utils/scores.R
+++ R/utils/scores.R
@@ -44,50 +44,50 @@ appendTBSscores <- function(D){
     TBS1S <- TBS2Sa <- TBS2Sb <- 0;
 
     ## one-step algorithm
-    if(Contact_with_adult_TB_case==1) TBS1S <- TBS1S + 8;
-    if(Cough_more_3w==1) TBS1S <- TBS1S + 6;
-    if(Loss_of_appetite_2w==1) TBS1S <- TBS1S + 2;
-    if(Temperature_more_38==1) TBS1S <- TBS1S + 2;
-    if(Tachycardia==1) TBS1S <- TBS1S + 5;
-    if(Chest_in_drawing==1) TBS1S <- TBS1S + 8;
-    if(Lack_of_consciousness==1) TBS1S <- TBS1S + 2;
-    if(Cervical_or_supra_clavicular_adenopathy==1) TBS1S <- TBS1S + 12;
-    if(CXR_Alveolar_opacity==1) TBS1S <- TBS1S + 4;
-    if(CXR_Hila_mediastinal_lymphadenopathy==1) TBS1S <- TBS1S + 6;
-    if(CXR_Pleural_effusion==1) TBS1S <- TBS1S + 13;
-    if(CXR_Pericardial_effusion==1) TBS1S <- TBS1S - 9;
-    if(AUS_Splenic_micro_abscesses==1) TBS1S <- TBS1S + 11;
-    if(AUS_Pericardial_or_pleural_effusion==1) TBS1S <- TBS1S + 5;
-    if(HIV_infection==1) TBS1S <- TBS1S + 6;
-    if(Xpert_Positive==1) TBS1S <- TBS1S + 40;
+    if(itb_exp_con.factor==1) TBS1S <- TBS1S + 8;
+    if(itb_cou_3==1) TBS1S <- TBS1S + 6;
+    if(itb_app_2==1) TBS1S <- TBS1S + 2;
+    if(temp_38==1) TBS1S <- TBS1S + 2;
+    if(tachycardia==1) TBS1S <- TBS1S + 5;
+    if(ice_ind.factor==1) TBS1S <- TBS1S + 8;
+    if(ice_csc.factor==1) TBS1S <- TBS1S + 2;
+    if(ice_ade.factor==1) TBS1S <- TBS1S + 12;
+    if(cxr_pre_alv.factor==1) TBS1S <- TBS1S + 4;
+    if(cxr_pre_hil.factor==1) TBS1S <- TBS1S + 6;
+    if(cxr_pre_ple.factor==1) TBS1S <- TBS1S + 13;
+    if(cxr_pre_eff.factor==1) TBS1S <- TBS1S - 9;
+    if(aus_sma.factor==1) TBS1S <- TBS1S + 11;
+    if(aus_effusion==1) TBS1S <- TBS1S + 5;
+    if(hiv_res.factor==1) TBS1S <- TBS1S + 6;
+    if(Xpert_res==1) TBS1S <- TBS1S + 40;
 
     ## two-step part 1
-    if(Contact_with_adult_TB_case==1) TBS2Sa <- TBS2Sa + 20;
-    if(Fatigue_more_2w==1) TBS2Sa <- TBS2Sa + 6;
-    if(Cough_more_3w==1) TBS2Sa <- TBS2Sa + 9;
-    if(Temperature_more_38==1) TBS2Sa <- TBS2Sa + 19;
-    if(Chest_in_drawing==1) TBS2Sa <- TBS2Sa + 9;
-    if(Crackles_on_auscultation==1) TBS2Sa <- TBS2Sa + 9;
-    if(Lack_of_consciousness==1) TBS2Sa <- TBS2Sa + 7;
-    if(Cervical_or_supra_clavicular_adenopathy==1) TBS2Sa <- TBS2Sa + 26;
-    if(HIV_infection_status==1) TBS2Sa <- TBS2Sa + 9;
+    if(itb_exp_con.factor==1) TBS2Sa <- TBS2Sa + 20;
+    if(itb_fat_2==1) TBS2Sa <- TBS2Sa + 6;
+    if(itb_cou_3==1) TBS2Sa <- TBS2Sa + 9;
+    if(temp_38==1) TBS2Sa <- TBS2Sa + 19;
+    if(ice_ind.factor==1) TBS2Sa <- TBS2Sa + 9;
+    if(ice_cra.factor==1) TBS2Sa <- TBS2Sa + 9;
+    if(ice_csc.factor==1) TBS2Sa <- TBS2Sa + 7;
+    if(ice_ade.factor==1) TBS2Sa <- TBS2Sa + 26;
+    if(hiv_res.factor==1) TBS2Sa <- TBS2Sa + 9;
 
     ## two-step part 2
-    if(Contact_with_adult_TB_case==1) TBS2Sa <- TBS2Sa + 9;
-    if(Cough_more_3w==1) TBS2Sa <- TBS2Sa + 5;
-    if(Loss_of_appetite_2w==1) TBS2Sa <- TBS2Sa + 2;
-    if(Temperature_more_38==1) TBS2Sa <- TBS2Sa + 4;
-    if(Chest_in_drawing==1) TBS2Sa <- TBS2Sa + 9;
-    if(Cervical_or_supra_clavicular_adenopathy==1) TBS2Sa <- TBS2Sa + 14;
-    if(CXR_Alveolar_opacity==1) TBS2Sa <- TBS2Sa + 4;
-    if(CXR_Hila_mediastinal_lymphadenopathy==1) TBS2Sa <- TBS2Sa + 6;
-    if(CXR_Pleural_effusion==1) TBS2Sa <- TBS2Sa + 13;
-    if(CXR_Pericardial_effusion==1) TBS2Sa <- TBS2Sa - 10;
-    if(AUS_Hepatomegaly==1) TBS2Sa <- TBS2Sa - 2;
-    if(AUS_Splenic_micro_abscesses==1) TBS2Sa <- TBS2Sa + 11;
-    if(AUS_Pericardial_or_pleural_effusion==1) TBS2Sa <- TBS2Sa + 7;
-    if(HIV_infection==1) TBS2Sa <- TBS2Sa + 7;
-    if(Xpert_Positive==1) TBS2Sa <- TBS2Sa + 41;
+    if(itb_exp_con.factor==1) TBS2Sb <- TBS2Sb + 9;
+    if(itb_cou_3==1) TBS2Sb <- TBS2Sb + 5;
+    if(itb_app_2==1) TBS2Sb <- TBS2Sb + 2;
+    if(temp_38==1) TBS2Sb <- TBS2Sb + 4;
+    if(ice_ind.factor==1) TBS2Sb <- TBS2Sb + 9;
+    if(ice_ade.factor==1) TBS2Sb <- TBS2Sb + 14;
+    if(cxr_pre_alv.factor==1) TBS2Sb <- TBS2Sb + 4;
+    if(cxr_pre_hil.factor==1) TBS2Sb <- TBS2Sb + 6;
+    if(cxr_pre_ple.factor==1) TBS2Sb <- TBS2Sb + 13;
+    if(cxr_pre_eff.factor==1) TBS2Sb <- TBS2Sb - 10;
+    if(aus_hep.factor==1) TBS2Sb <- TBS2Sb - 2;
+    if(aus_sma.factor==1) TBS2Sb <- TBS2Sb + 11;
+    if(aus_effusion==1) TBS2Sb <- TBS2Sb + 7;
+    if(hiv_res.factor==1) TBS2Sb <- TBS2Sb + 7;
+    if(Xpert_res==1) TBS2Sb <- TBS2Sb + 41;
 
     ## return
     list(TBS1S, TBS2Sa, TBS2Sb)
