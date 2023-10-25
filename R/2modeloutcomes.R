6637072fae392b0f2538da1cd0ea088f82198bdf
Marc d'Elbée <marc.delbee@u-bordeaux.fr>
2023-10-25 09:16:21 +0200

    comment out R code blocks on real SAM cohort population

--- R/2modeloutcomes.R
+++ R/2modeloutcomes.R
@@ -14,8 +14,8 @@ source(gh('R/utils/costutils.R')) #cost data parser
 source(here('R/utils/readyoutcomes.R')) #parameters & life-years
 source(here('R/utils/HEoutputs.R')) #various outputters
 
-realpopt <- read_excel("data/realpopt.xlsx")
-realpop <- read_excel("data/realpop.xlsx")
+#realpopt <- read_excel("data/realpopt.xlsx")
+#realpop <- read_excel("data/realpop.xlsx")
 
 
 ## load synthetic populations
@@ -56,124 +56,124 @@ popt[,mean(TBS1S>=10)] #sensitivity =83%     clinical paper: TBS1S sen: 85% [77-
 popt[,mean(TBS2Sa>=10 & TBS2Sb>=10, na.rm = TRUE)] #sensitivity =76%      clinical paper: TBS2S sen: 77% [68-84] 
 
 
-
+##
 ## Compare synthetic and real populations 
 
 ## add in scores to real populations
 
-# TB pop
-names(realpopt)[names(realpopt) == "pat_ide"] <- "id"
-
-realpopt$Contact_TB<-ifelse(realpopt$Contact_TB=="Yes",1,0)
-realpopt$itb_fat_2<-ifelse(realpopt$itb_fat_2=="Yes",1,0)
-realpopt$itb_fev_2<-ifelse(realpopt$itb_fev_2=="Yes",1,0)
-realpopt$itb_cou_3<-ifelse(realpopt$itb_cou_3=="Yes",1,0)
-realpopt$itb_app_2<-ifelse(realpopt$itb_app_2=="Yes",1,0)
-realpopt$temp_38<-ifelse(realpopt$temp_38=="Yes",1,0)
-realpopt$itb_wgt.factor<-ifelse(realpopt$itb_wgt.factor=="Yes",1,0)
-realpopt$tachycardia<-ifelse(realpopt$tachycardia=="Yes",1,0)
-realpopt$tachypnea<-ifelse(realpopt$tachypnea=="Yes",1,0)
-realpopt$ice_ind_bin.factor<-ifelse(realpopt$ice_ind_bin.factor=="Yes",1,0)
-realpopt$ice_cra.factor<-ifelse(realpopt$ice_cra.factor=="Yes",1,0)
-realpopt$Dep_csc<-ifelse(realpopt$Dep_csc=="Yes",1,0)
-realpopt$ice_ade_bin.factor<-ifelse(realpopt$ice_ade_bin.factor=="Yes",1,0)
-realpopt$cxr_pre_mil.factor<-ifelse(realpopt$cxr_pre_mil.factor=="Yes",1,0)
-realpopt$cxr_pre_alv.factor<-ifelse(realpopt$cxr_pre_alv.factor=="Yes",1,0)
-realpopt$cxr_pre_hil.factor<-ifelse(realpopt$cxr_pre_hil.factor=="Yes",1,0)
-realpopt$cxr_pre_exc.factor<-ifelse(realpopt$cxr_pre_exc.factor=="Yes",1,0)
-realpopt$cxr_pre_ple.factor<-ifelse(realpopt$cxr_pre_ple.factor=="Yes",1,0)
-realpopt$cxr_pre_eff.factor<-ifelse(realpopt$cxr_pre_eff.factor=="Yes",1,0)
-realpopt$cxr_pre_ple_per_eff.factor<-ifelse(realpopt$cxr_pre_ple_per_eff.factor=="Yes",1,0)
-realpopt$aus_sma.factor<-ifelse(realpopt$aus_sma.factor=="Yes",1,0)
-realpopt$aus_hma.factor<-ifelse(realpopt$aus_hma.factor=="Yes",1,0)
-realpopt$aus_effusion<-ifelse(realpopt$aus_effusion=="Yes",1,0)
-realpopt$aus_asc.factor<-ifelse(realpopt$aus_asc.factor=="Yes",1,0)
-
-realpopt$hiv_res.factor<-ifelse(realpopt$hiv_res.factor=="Positive",1,0)
-realpopt$Xpert_res<-ifelse(realpopt$Xpert_res=="Positive",1,0)
-
-#Not TB pop
-names(realpop)[names(realpop) == "pat_ide"] <- "id"
-
-realpop$Contact_TB<-ifelse(realpop$Contact_TB=="Yes",1,0)
-realpop$itb_fat_2<-ifelse(realpop$itb_fat_2=="Yes",1,0)
-realpop$itb_fev_2<-ifelse(realpop$itb_fev_2=="Yes",1,0)
-realpop$itb_cou_3<-ifelse(realpop$itb_cou_3=="Yes",1,0)
-realpop$itb_app_2<-ifelse(realpop$itb_app_2=="Yes",1,0)
-realpop$temp_38<-ifelse(realpop$temp_38=="Yes",1,0)
-realpop$itb_wgt.factor<-ifelse(realpop$itb_wgt.factor=="Yes",1,0)
-realpop$tachycardia<-ifelse(realpop$tachycardia=="Yes",1,0)
-realpop$tachypnea<-ifelse(realpop$tachypnea=="Yes",1,0)
-realpop$ice_ind_bin.factor<-ifelse(realpop$ice_ind_bin.factor=="Yes",1,0)
-realpop$ice_cra.factor<-ifelse(realpop$ice_cra.factor=="Yes",1,0)
-realpop$Dep_csc<-ifelse(realpop$Dep_csc=="Yes",1,0)
-realpop$ice_ade_bin.factor<-ifelse(realpop$ice_ade_bin.factor=="Yes",1,0)
-realpop$cxr_pre_mil.factor<-ifelse(realpop$cxr_pre_mil.factor=="Yes",1,0)
-realpop$cxr_pre_alv.factor<-ifelse(realpop$cxr_pre_alv.factor=="Yes",1,0)
-realpop$cxr_pre_hil.factor<-ifelse(realpop$cxr_pre_hil.factor=="Yes",1,0)
-realpop$cxr_pre_exc.factor<-ifelse(realpop$cxr_pre_exc.factor=="Yes",1,0)
-realpop$cxr_pre_ple.factor<-ifelse(realpop$cxr_pre_ple.factor=="Yes",1,0)
-realpop$cxr_pre_eff.factor<-ifelse(realpop$cxr_pre_eff.factor=="Yes",1,0)
-realpop$cxr_pre_ple_per_eff.factor<-ifelse(realpop$cxr_pre_ple_per_eff.factor=="Yes",1,0)
-realpop$aus_sma.factor<-ifelse(realpop$aus_sma.factor=="Yes",1,0)
-realpop$aus_hma.factor<-ifelse(realpop$aus_hma.factor=="Yes",1,0)
-realpop$aus_effusion<-ifelse(realpop$aus_effusion=="Yes",1,0)
-realpop$aus_asc.factor<-ifelse(realpop$aus_asc.factor=="Yes",1,0)
-
-realpop$hiv_res.factor<-ifelse(realpop$hiv_res.factor=="Positive",1,0)
-realpop$Xpert_res<-ifelse(realpop$Xpert_res=="Positive",1,0)
-
-
-## --- TB Speed
-setDT(realpop)
-setDT(realpopt)
-
-realpop <- appendTBSscores(realpop)
-realpopt <- appendTBSscores(realpopt)
-
-
-## check se/sp using Minh's variables
-realpop[,1-mean(realpop$SCO_ORG_tot>=10)] #specificity =81%    clinical paper: TBS1S spe: 82%
-realpop[,1-mean(realpop$SCO_SCR_tot>=10 & realpop$SCO_DIA_tot>=10)] #specificity =84%     clinical paper: TBS2S spe: 86%
-realpopt[,mean(realpopt$SCO_ORG_tot>=10)] #sensitivity =86%     clinical paper: TBS1S sen: 85%
-realpopt[,mean(realpopt$SCO_SCR_tot>=10 & realpopt$SCO_DIA_tot>=10)] #sensitivity =79%      clinical paper: TBS2S sen: 77% 
-
-
-
-## check se/sp using our score
-realpop[,1-mean(TBS1S>=10)] #real pop sp = 81% 
-                           #synthetic pop sp: 80% 
-                           #clinical paper sp: 82% [78-85]
-realpop[,1-mean(TBS2Sa>=10 & TBS2Sb>=10, na.rm = TRUE)] #real pop sp = 84% 
-                                                      #synthetic pop sp: 85% 
-                                                      #clinical paper sp: 86% [82-89]
-realpopt[,mean(TBS1S>=10)] #real pop se = 86% 
-                          #synthetic pop se: 83% 
-                          #clinical paper se: 85% [77-91]
-realpopt[,mean(TBS2Sa>=10 & TBS2Sb>=10, na.rm = TRUE)] #real pop se = 79% 
-                                                     #synthetic pop se: 76% 
-                                                     #clinical paper se: 77% [68-84]
-
-table(realpopt$SCO_ORG_tot>=10) # 87/101
-table(realpop$SCO_ORG_tot<10) # 351/434
-
-table(realpopt$SCO_SCR_tot>=10) # 90/101
-table(realpop$SCO_SCR_tot<10) # 151/434
-
-table(realpopt$SCO_DIA_tot>=10) # 80/101
-table(realpop$SCO_DIA_tot<10) # 212+151= 363/434
-summary(is.na(realpop$SCO_DIA_tot))
-
-
-#Compare Minh's scores and ours
-all(realpopt$SCO_ORG_tot == realpopt$TBS1S)
-all(realpop$SCO_ORG_tot == realpop$TBS1S)
-
-all(realpopt$SCO_SCR_tot == realpopt$TBS2Sa)
-all(realpop$SCO_SCR_tot == realpop$TBS2Sa)
-
-all(realpopt$SCO_DIA_tot == realpopt$TBS2Sb, na.rm = TRUE)
-all(realpop$SCO_DIA_tot == realpop$TBS2Sb, na.rm = TRUE)
+# # TB pop
+# names(realpopt)[names(realpopt) == "pat_ide"] <- "id"
+# 
+# realpopt$Contact_TB<-ifelse(realpopt$Contact_TB=="Yes",1,0)
+# realpopt$itb_fat_2<-ifelse(realpopt$itb_fat_2=="Yes",1,0)
+# realpopt$itb_fev_2<-ifelse(realpopt$itb_fev_2=="Yes",1,0)
+# realpopt$itb_cou_3<-ifelse(realpopt$itb_cou_3=="Yes",1,0)
+# realpopt$itb_app_2<-ifelse(realpopt$itb_app_2=="Yes",1,0)
+# realpopt$temp_38<-ifelse(realpopt$temp_38=="Yes",1,0)
+# realpopt$itb_wgt.factor<-ifelse(realpopt$itb_wgt.factor=="Yes",1,0)
+# realpopt$tachycardia<-ifelse(realpopt$tachycardia=="Yes",1,0)
+# realpopt$tachypnea<-ifelse(realpopt$tachypnea=="Yes",1,0)
+# realpopt$ice_ind_bin.factor<-ifelse(realpopt$ice_ind_bin.factor=="Yes",1,0)
+# realpopt$ice_cra.factor<-ifelse(realpopt$ice_cra.factor=="Yes",1,0)
+# realpopt$Dep_csc<-ifelse(realpopt$Dep_csc=="Yes",1,0)
+# realpopt$ice_ade_bin.factor<-ifelse(realpopt$ice_ade_bin.factor=="Yes",1,0)
+# realpopt$cxr_pre_mil.factor<-ifelse(realpopt$cxr_pre_mil.factor=="Yes",1,0)
+# realpopt$cxr_pre_alv.factor<-ifelse(realpopt$cxr_pre_alv.factor=="Yes",1,0)
+# realpopt$cxr_pre_hil.factor<-ifelse(realpopt$cxr_pre_hil.factor=="Yes",1,0)
+# realpopt$cxr_pre_exc.factor<-ifelse(realpopt$cxr_pre_exc.factor=="Yes",1,0)
+# realpopt$cxr_pre_ple.factor<-ifelse(realpopt$cxr_pre_ple.factor=="Yes",1,0)
+# realpopt$cxr_pre_eff.factor<-ifelse(realpopt$cxr_pre_eff.factor=="Yes",1,0)
+# realpopt$cxr_pre_ple_per_eff.factor<-ifelse(realpopt$cxr_pre_ple_per_eff.factor=="Yes",1,0)
+# realpopt$aus_sma.factor<-ifelse(realpopt$aus_sma.factor=="Yes",1,0)
+# realpopt$aus_hma.factor<-ifelse(realpopt$aus_hma.factor=="Yes",1,0)
+# realpopt$aus_effusion<-ifelse(realpopt$aus_effusion=="Yes",1,0)
+# realpopt$aus_asc.factor<-ifelse(realpopt$aus_asc.factor=="Yes",1,0)
+# 
+# realpopt$hiv_res.factor<-ifelse(realpopt$hiv_res.factor=="Positive",1,0)
+# realpopt$Xpert_res<-ifelse(realpopt$Xpert_res=="Positive",1,0)
+# 
+# #Not TB pop
+# names(realpop)[names(realpop) == "pat_ide"] <- "id"
+# 
+# realpop$Contact_TB<-ifelse(realpop$Contact_TB=="Yes",1,0)
+# realpop$itb_fat_2<-ifelse(realpop$itb_fat_2=="Yes",1,0)
+# realpop$itb_fev_2<-ifelse(realpop$itb_fev_2=="Yes",1,0)
+# realpop$itb_cou_3<-ifelse(realpop$itb_cou_3=="Yes",1,0)
+# realpop$itb_app_2<-ifelse(realpop$itb_app_2=="Yes",1,0)
+# realpop$temp_38<-ifelse(realpop$temp_38=="Yes",1,0)
+# realpop$itb_wgt.factor<-ifelse(realpop$itb_wgt.factor=="Yes",1,0)
+# realpop$tachycardia<-ifelse(realpop$tachycardia=="Yes",1,0)
+# realpop$tachypnea<-ifelse(realpop$tachypnea=="Yes",1,0)
+# realpop$ice_ind_bin.factor<-ifelse(realpop$ice_ind_bin.factor=="Yes",1,0)
+# realpop$ice_cra.factor<-ifelse(realpop$ice_cra.factor=="Yes",1,0)
+# realpop$Dep_csc<-ifelse(realpop$Dep_csc=="Yes",1,0)
+# realpop$ice_ade_bin.factor<-ifelse(realpop$ice_ade_bin.factor=="Yes",1,0)
+# realpop$cxr_pre_mil.factor<-ifelse(realpop$cxr_pre_mil.factor=="Yes",1,0)
+# realpop$cxr_pre_alv.factor<-ifelse(realpop$cxr_pre_alv.factor=="Yes",1,0)
+# realpop$cxr_pre_hil.factor<-ifelse(realpop$cxr_pre_hil.factor=="Yes",1,0)
+# realpop$cxr_pre_exc.factor<-ifelse(realpop$cxr_pre_exc.factor=="Yes",1,0)
+# realpop$cxr_pre_ple.factor<-ifelse(realpop$cxr_pre_ple.factor=="Yes",1,0)
+# realpop$cxr_pre_eff.factor<-ifelse(realpop$cxr_pre_eff.factor=="Yes",1,0)
+# realpop$cxr_pre_ple_per_eff.factor<-ifelse(realpop$cxr_pre_ple_per_eff.factor=="Yes",1,0)
+# realpop$aus_sma.factor<-ifelse(realpop$aus_sma.factor=="Yes",1,0)
+# realpop$aus_hma.factor<-ifelse(realpop$aus_hma.factor=="Yes",1,0)
+# realpop$aus_effusion<-ifelse(realpop$aus_effusion=="Yes",1,0)
+# realpop$aus_asc.factor<-ifelse(realpop$aus_asc.factor=="Yes",1,0)
+# 
+# realpop$hiv_res.factor<-ifelse(realpop$hiv_res.factor=="Positive",1,0)
+# realpop$Xpert_res<-ifelse(realpop$Xpert_res=="Positive",1,0)
+# 
+# 
+# ## --- TB Speed
+# setDT(realpop)
+# setDT(realpopt)
+# 
+# realpop <- appendTBSscores(realpop)
+# realpopt <- appendTBSscores(realpopt)
+# 
+# 
+# ## check se/sp using Minh's variables
+# realpop[,1-mean(realpop$SCO_ORG_tot>=10)] #specificity =81%    clinical paper: TBS1S spe: 82%
+# realpop[,1-mean(realpop$SCO_SCR_tot>=10 & realpop$SCO_DIA_tot>=10)] #specificity =84%     clinical paper: TBS2S spe: 86%
+# realpopt[,mean(realpopt$SCO_ORG_tot>=10)] #sensitivity =86%     clinical paper: TBS1S sen: 85%
+# realpopt[,mean(realpopt$SCO_SCR_tot>=10 & realpopt$SCO_DIA_tot>=10)] #sensitivity =79%      clinical paper: TBS2S sen: 77% 
+# 
+# 
+# 
+# ## check se/sp using our score
+# realpop[,1-mean(TBS1S>=10)] #real pop sp = 81% 
+#                            #synthetic pop sp: 80% 
+#                            #clinical paper sp: 82% [78-85]
+# realpop[,1-mean(TBS2Sa>=10 & TBS2Sb>=10, na.rm = TRUE)] #real pop sp = 84% 
+#                                                       #synthetic pop sp: 85% 
+#                                                       #clinical paper sp: 86% [82-89]
+# realpopt[,mean(TBS1S>=10)] #real pop se = 86% 
+#                           #synthetic pop se: 83% 
+#                           #clinical paper se: 85% [77-91]
+# realpopt[,mean(TBS2Sa>=10 & TBS2Sb>=10, na.rm = TRUE)] #real pop se = 79% 
+#                                                      #synthetic pop se: 76% 
+#                                                      #clinical paper se: 77% [68-84]
+# 
+# table(realpopt$SCO_ORG_tot>=10) # 87/101
+# table(realpop$SCO_ORG_tot<10) # 351/434
+# 
+# table(realpopt$SCO_SCR_tot>=10) # 90/101
+# table(realpop$SCO_SCR_tot<10) # 151/434
+# 
+# table(realpopt$SCO_DIA_tot>=10) # 80/101
+# table(realpop$SCO_DIA_tot<10) # 212+151= 363/434
+# summary(is.na(realpop$SCO_DIA_tot))
+# 
+# 
+# #Compare Minh's scores and ours
+# all(realpopt$SCO_ORG_tot == realpopt$TBS1S)
+# all(realpop$SCO_ORG_tot == realpop$TBS1S)
+# 
+# all(realpopt$SCO_SCR_tot == realpopt$TBS2Sa)
+# all(realpop$SCO_SCR_tot == realpop$TBS2Sa)
+# 
+# all(realpopt$SCO_DIA_tot == realpopt$TBS2Sb, na.rm = TRUE)
+# all(realpop$SCO_DIA_tot == realpop$TBS2Sb, na.rm = TRUE)
 
 
 
